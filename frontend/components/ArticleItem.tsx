d1de60ccfd1c519e001317babea04ed0ad16559f
Konrad Marzec <konrad.marzec1991@gmail.com>
2022-11-08 00:58:04 +0100

    Add CardWrapper

--- frontend/components/ArticleItem.tsx
+++ frontend/components/ArticleItem.tsx
@@ -1,3 +1,4 @@
+import {useState} from "react";
 import {Card} from "react-bootstrap";
 
 
@@ -8,9 +9,18 @@ type ArticleItemProps = {
     imgUrl: string
 }
 
-export const ArticleItem = ({id, title, author, imgUrl}: ArticleItemProps) => {
+const CardWrapper = ({id, title, author, imgUrl}: ArticleItemProps) => {
+    const [hovered, setHovered] = useState(false);
+
     return (
-        <Card>
+        <Card
+            onMouseOver={() => setHovered(true)}
+            onMouseOut={() => setHovered(false)}
+            style={{
+                opacity: hovered ? 0.9 : 1,
+                transitionTimingFunction: "cubic-bezier(0.1, 0.7, 1.0, 0.1)"
+            }}
+        >
             <Card.Img variant="top" src={imgUrl} height="200px" style={{ objectFit: "cover" }}/>
             <Card.Body className="d-flex flex-column">
                 <Card.Title className="d-flex justify-content-between align-items-baseline mb-4">
@@ -19,5 +29,12 @@ export const ArticleItem = ({id, title, author, imgUrl}: ArticleItemProps) => {
                 </Card.Title>
             </Card.Body>
         </Card>
+    )
+
+}
+
+export const ArticleItem = (props: ArticleItemProps) => {
+    return (
+        <CardWrapper {...props}/>
     )
 }
