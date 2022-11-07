39b656056b63401da2caa86c6818a93115a6814f
Konrad Marzec <konrad.marzec1991@gmail.com>
2022-11-07 14:31:25 +0100

    Add pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,38 @@
-import React from 'react';
+import React from "react";
+import {Routes, Route, BrowserRouter} from "react-router-dom";
+import {Container, Nav} from "react-bootstrap";
+import {Home} from "../pages/Home";
+import {Articles} from "../pages/Articles";
+import {About} from "../pages/About";
+import {Navbar} from "react-bootstrap";
+import { NavLink } from "react-router-dom"
 
-function App() {
+
+export const App = (): React.ReactElement => {
   return (
-    <h1>Hello</h1>
-  );
+      <Container>
+        <Navbar>
+            <Container>
+                <Nav className="me-auto">
+                  <Nav.Link to="/" as={NavLink}>
+                    Home
+                  </Nav.Link>
+                  <Nav.Link to="/store" as={NavLink}>
+                    Store
+                  </Nav.Link>
+                  <Nav.Link to="/about" as={NavLink}>
+                    About
+                  </Nav.Link>
+                </Nav>
+            </Container>
+        </Navbar>
+        <BrowserRouter>
+          <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/articles" element={<Articles />} />
+              <Route path="/about" element={<About />} />
+          </Routes>
+        </BrowserRouter>
+      </Container>
+  )
 }
-
-export default App;
