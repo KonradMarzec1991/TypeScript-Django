6afe321b4ee9d88c01a683cf6ecaa384681bc90e
Konrad Marzec <konrad.marzec1991@gmail.com>
2022-11-12 21:01:20 +0100

    Use UUID

--- backend/article/schema.py
+++ backend/article/schema.py
@@ -6,38 +6,21 @@ from graphene_django.filter import DjangoFilterConnectionField
 
 from .filters import ArticleFilter
 from .models import Article
-
-import graphene
-
-
-class ExtendedConnection(graphene.Connection):
-    class Meta:
-        abstract = True
-
-    total_count = graphene.Int()
-    edge_count = graphene.Int()
-
-    def resolve_total_count(self, info, **kwargs):
-        return self.length
-
-    def resolve_edge_count(self, info, **kwargs):
-        return len(self.edges)
+from graphql_relay import to_global_id
 
 
-
-class UserType(DjangoObjectType):
+class UserNode(DjangoObjectType):
     class Meta:
         model = get_user_model()
 
 
-class ArticleType(DjangoObjectType):
+class ArticleNode(DjangoObjectType):
     class Meta:
         model = Article
         filterset_class = ArticleFilter
         interfaces = (relay.Node, )
-        connection_class = ExtendedConnection
 
 
 class Query(graphene.ObjectType):
-    article = relay.Node.Field(ArticleType)
-    all_articles = DjangoFilterConnectionField(ArticleType, required=False)
+    article = relay.Node.Field(ArticleNode)
+    all_articles = DjangoFilterConnectionField(ArticleNode, required=False)
