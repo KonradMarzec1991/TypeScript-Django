7e8de9ede4dbab3718a0d4f91a9983c0b2614173
Konrad Marzec <konrad.marzec1991@gmail.com>
2022-11-12 02:10:13 +0100

    Remove ExtendedConnection

--- backend/article/schema.py
+++ backend/article/schema.py
@@ -7,23 +7,6 @@ from graphene_django.filter import DjangoFilterConnectionField
 from .filters import ArticleFilter
 from .models import Article
 
-import graphene
-
-
-class ExtendedConnection(graphene.Connection):
-    class Meta:
-        abstract = True
-
-    total_count = graphene.Int()
-    edge_count = graphene.Int()
-
-    def resolve_total_count(self, info, **kwargs):
-        return self.length
-
-    def resolve_edge_count(self, info, **kwargs):
-        return len(self.edges)
-
-
 
 class UserType(DjangoObjectType):
     class Meta:
@@ -35,7 +18,6 @@ class ArticleType(DjangoObjectType):
         model = Article
         filterset_class = ArticleFilter
         interfaces = (relay.Node, )
-        connection_class = ExtendedConnection
 
 
 class Query(graphene.ObjectType):
