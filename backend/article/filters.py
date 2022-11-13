376027e4fabc7d1c1efd0bd2ed46052afa528083
Konrad Marzec <konrad.marzec1991@gmail.com>
2022-11-13 19:06:53 +0100

    Add search to gql query

--- backend/article/filters.py
+++ backend/article/filters.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django_filters import (
     CharFilter,
     ChoiceFilter,
@@ -8,10 +9,21 @@ from .models import Article
 
 
 class ArticleFilter(FilterSet):
+    search = CharFilter(method="search_filter")
     title = CharFilter(field_name="title", lookup_expr="istartswith")
-    category = ChoiceFilter(field_name="category", choices=Article.CHOICES)
     authors = CharFilter(field_name="authors__username")
+    category = ChoiceFilter(choices=Article.CHOICES)
 
     class Meta:
         fields = ("title", "category", "authors")
         model = Article
+
+    def search_filter(self, qs, name, value):
+        print(value)
+        if value:
+            return qs.filter(
+                Q(title__icontains=value) |
+                Q(category__icontains=value) |
+                Q(authors__username__icontains=value)
+            )
+        return qs
