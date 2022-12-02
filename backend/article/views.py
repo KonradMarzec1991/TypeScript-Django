aa893ccefac00a2e09ce71353cd0d9293b563658
Konrad Marzec <konrad.marzec1991@gmail.com>
2022-12-02 19:43:12 +0100

    Add api skeleton

--- backend/article/views.py
+++ backend/article/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+from .serializers import ArticleSerializer
+
+
+class ArticleListView(APIView):
+    def get(self, request, *args, **kwargs):
+        pass
+
+    def post(self, request, *args, **kwargs):
+        pass
+
+
+class ArticleDetailView(APIView):
+    def get(self, request, pk, *args, **kwargs):
+        pass
+
+    def put(self, request, pk, *args, **kwargs):
+        pass
+
+    def delete(self, request, pk, *args, **kwargs):
+        pass
