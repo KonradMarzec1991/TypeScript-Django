47f4edaff4353a10a0e6e515b099ff5e36e73d6c
Konrad Marzec <konrad.marzec1991@gmail.com>
2022-12-02 20:51:44 +0100

    Serialize author

--- backend/article/serializers.py
+++ backend/article/serializers.py
@@ -1,9 +1,18 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
 
 from article.models import Article
 
 
+class AuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["id", "username", "first_name", "last_name", "email"]
+
+
 class ArticleSerializer(serializers.ModelSerializer):
+    authors = AuthorSerializer(many=True)
+
     class Meta:
         model = Article
         fields = "__all__"
